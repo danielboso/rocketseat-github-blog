1e1ae0e857c33bc5232d8205d8d1d7d31c6d24c1
Daniel Boso <bosodaniel@hotmail.com>
2022-12-08 21:59:57 -0300

    style: Config Styled Components

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  'BLUE': '#3294F8',
+  'BASE_TITLE': '#E7EDF4',
+  'BASE_SUBTITLE': '#C4D4E3',
+  'BASE_TEXT': '#AFC2D4',
+  'BASE_SPAN': '#7B96B2',
+  'BASE_LABEL': '#3A536B',
+  'BASE_BORDER': '#1C2F41',
+  'BASE_POST': '#112131',
+  'BASE_PROFILE': '#0B1B2B',
+  'BASE_BACKGROUND': '#071422',
+  'BASE_INPUT': '#040F1A',
+} as const
