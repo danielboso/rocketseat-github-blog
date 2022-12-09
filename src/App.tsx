11ed55d43b41250fe7c7b2595092050a385d697e
Daniel Boso <bosodaniel@hotmail.com>
2022-12-08 21:37:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="https://via.placeholder.com/148" alt="" />
+        <div>
+          <div>
+            <h1>Cameron Williamson</h1>
+            <a href="">github</a>
+          </div>
+          <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+          <div>
+            <p>cameronwll</p>
+            <p>Rocketseat</p>
+            <p>32 seguidores</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>Publicações</h2>
+          <span>6 publicações</span>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+        <div>
+          <div>
+            <p>JavaScript data types and data structures</p>
+            <span>Há 1 dia</span>
+          </div>
+          <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <span>voltar</span>
+          <span>ver no github</span>
+        </div>
+        <h1>JavaScript data types and data structures</h1>
+        <div>
+          <span>cameronwll</span>
+          <span>Há 1 dia</span>
+          <span>5 comentários</span>
+        </div>
+      </div>
+
+      <div>
+        <p>Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.</p>
+
+        <a href="">Dynamic typing</a>
+        <p>JavaScript is a loosely typed and dynamic language. Variables in JavaScript are not directly associated with any particular value type, and any variable can be assigned (and re-assigned) values of all types:</p>
+
+        <p>let foo = 42;   // foo is now a number</p>
+        <p>foo = ‘bar’;    // foo is now a string</p>
+        <p>foo = true;     // foo is now a boolean</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
